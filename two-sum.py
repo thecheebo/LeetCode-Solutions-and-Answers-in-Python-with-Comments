851e7b30fc746ec4ec7dd881f11a88cabc3fe3f2
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-05-31 16:59:11 -0700

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -1,12 +1,19 @@
 class Solution(object):
     def twoSum(self, nums, target):
         
-        # Create 
+        # Create an empty dictionary
         d = {}
    
+        # Iterate through Array with an Indexable Value
         for i, n in enumerate(nums):
+            
+            # Get the value you are matching target
             m = target - n
+            
+            # Search if its in the dictionary
             if m in d:
                 return [i, d[m]]
+            
+            # If it isn't then add this value with the current key with the index as its value
             else:
                 d[n] = i
