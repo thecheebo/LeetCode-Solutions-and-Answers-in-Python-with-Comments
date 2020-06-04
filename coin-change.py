76b92917d0df0723680304242a5083fb2feba4a3
thecheebo <59412572+thecheebo@users.noreply.github.com>
2020-06-04 12:27:19 -0700

    Create coin-change.py

---
+++ coin-change.py
@@ -0,0 +1,19 @@
+"""
+https://leetcode.com/problems/coin-change
+
+Dynamic Programming
+
+"""
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        
+        dp_array = [amount+1] * (amount + 1)
+        dp_array[0] = 0
+
+        for coin in coins:
+            for count in range(coin, amount+1):
+                dp_array[count] = min(dp_array[count], dp_array[count-coin]+1)
+        if dp_array[amount] != (amount + 1):
+            return dp_array[amount]
+        return -1
