c22404f9d7c24b22782e2df7cc7d906bc71943ca
thecheebo <59412572+thecheebo@users.noreply.github.com>
2021-09-02 17:20:38 -0700

    Time: 67 ms (62.95%), Space: 14.3 MB (76.53%) - LeetHub

--- combination-sum-ii/combination-sum-ii.py
+++ combination-sum-ii/combination-sum-ii.py
@@ -1,29 +1,29 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        def backtrack(comb, remain, curr, results):
-
-            if remain == 0:
-                # make a deep copy of the resulted combination
-                results.append(list(comb))
-                return
-
-            for next_curr in range(curr, len(candidates)):
-
-                if next_curr > curr \
-                  and candidates[next_curr] == candidates[next_curr-1]:
-                    continue
-
-                pick = candidates[next_curr]
-                # optimization: skip the rest of elements starting from 'curr' index
-                if remain - pick < 0:
-                    break
-
-                comb.append(pick)
-                backtrack(comb, remain - pick, next_curr + 1, results)
-                comb.pop()
-
         candidates.sort()
-        comb, results = [], []
-        backtrack(comb, target, 0, results)
+        def backtrack(current, remainder, index):
+            
+            if remainder == 0:
+                unique_combinations.append(current[:])
+                return
+            for i in range(index, len(candidates)):
+                if i > index and candidates[i] == candidates[i-1]:
+                    continue
+                if (remainder - candidates[i] >= 0):
+                    current.append(candidates[i])
 
-        return results
+                    backtrack(current, remainder - candidates[i], i+1)
+
+                    current.pop()
+
+                else:
+                    break
+            
+
+        
+        
+        unique_combinations = []
+        
+        
+        backtrack([],target, 0)
+        return unique_combinations
