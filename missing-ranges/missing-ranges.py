9801ba0332c8ff48c1893a52c9457f2fcd4fd20f
thecheebo <59412572+thecheebo@users.noreply.github.com>
2021-10-01 15:28:18 -0700

    Time: 40 ms (22.47%), Space: 14.3 MB (23.36%) - LeetHub

--- missing-ranges/missing-ranges.py
+++ missing-ranges/missing-ranges.py
@@ -2,13 +2,19 @@ class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
         result = []
         nums.append(upper+1)
-        pre = lower - 1
-        for i in nums:
-            if i < lower: continue
-            if min(i, upper+1) == pre + 2:
-                result.append(str(pre+1))
-            elif min(i, upper+1) > pre + 2:
-                result.append(str(pre + 1) + "->" + str(min(i, upper+1)-1))
-            if i >= upper: break
-            pre = i
+        prev_num = lower - 1
+        for num in nums:
+            # Base case if num in nums is lower than the LOWER given:
+            if num < lower: 
+                continue
+
+            # 2nd Base case if there is a num thats inbetween two nums:
+            if min(num, upper+1) == prev_num + 2:
+                result.append(str(prev_num+1))
+
+            # 3rd base case where there is a gap greater than two from the num and prev num
+            elif min(num, upper+1) > prev_num + 2:
+                result.append(str(prev_num + 1) + "->" + str(min(num, upper+1)-1))
+            if num >= upper: break
+            prev_num = num
         return result
