0296842b760f26c7654d7005361ff92d2d351c35
thecheebo <59412572+thecheebo@users.noreply.github.com>
2021-10-02 17:00:43 -0700

    Time: 696 ms (78.44%), Space: 19.3 MB (63.41%) - LeetHub

---
+++ course-schedule-iii/course-schedule-iii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def scheduleCourse(self, courses: List[List[int]]) -> int:
+
+        pq = []
+        start = 0
+        for t, end in sorted(courses, key = lambda x:x[1]):
+            start += t
+            heapq.heappush(pq, -t)
+            while start > end:
+                start += heapq.heappop(pq)
+        return len(pq)
